13edd263c83a226f7adba594587859374be742b6
KrishnaPulluru <85900329+krishnapulluru@users.noreply.github.com>
2022-08-16 21:43:23 +0530

    Project directory cleanded and main folders are created

---
+++ src/components/header/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./header.css";
+
+const Header = () => {
+    return (
+        <header className="header-cont">
+            <nav className="navigation">
+                <div>
+                    <h2>Simple Cart</h2>
+                </div>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
